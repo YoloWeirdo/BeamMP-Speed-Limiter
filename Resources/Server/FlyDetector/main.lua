ab5fa4c0b364659ccb08eb4e61e9f676fc836831
YoloWeirdo <66915906+YoloWeirdo@users.noreply.github.com>
2023-03-11 20:03:21 +0100

    Replaced return with continue
    Made it actually work with more than 1 person in the server, instead of just returning if 1 person doesn't have a car spawned. Just a small slight oversight I know.

--- Resources/Server/FlyDetector/main.lua
+++ Resources/Server/FlyDetector/main.lua
@@ -2,9 +2,9 @@
 WarnSpeed = 180
 KickSpeed = 200
 
-
-MP.RegisterEvent("MainEvent", "Main")
-MP.CreateEventTimer("MainEvent", 1000)
+MP.CancelEventTimer("FlyDetector", 1000)
+MP.RegisterEvent("FlyDetector", "Main")
+MP.CreateEventTimer("FlyDetector", 1000)
 
 Players, PlayerCars, PlayerVelocities, PlayerSpeeds = {}, {}, {}, {}
 
@@ -21,13 +21,13 @@ function Main()
     Players = MP.GetPlayers()
 
     for i, v in pairs(Players) do
-        PlayerCars = MP.GetPlayerVehicles(i)
-        if not PlayerCars then return end
+        PlayerCars[i] = MP.GetPlayerVehicles(i)
+        if not PlayerCars[i] then goto skipPlayer end
 
         for j, _ in pairs(PlayerCars) do
             if not PlayerVelocities[i] then PlayerVelocities[i] = {} end
 
-            if not MP.GetPositionRaw(i, j) then return end
+            if not MP.GetPositionRaw(i, j) then goto skipCar end
 
             PlayerVelocities[i][j] = MP.GetPositionRaw(i, j).vel
 
@@ -36,6 +36,9 @@ function Main()
             PlayerSpeeds[i][j] = math.floor(math.sqrt(PlayerVelocities[i][j][1]^2 + PlayerVelocities[i][j][2]^2 + PlayerVelocities[i][j][3]^2) * 2.2369362912)
 
             Judge(PlayerSpeeds[i][j], i)
+
+            ::skipCar::
         end
+        ::skipPlayer::
     end
 end
